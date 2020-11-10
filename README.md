6681ca4e16d940119cc2fbdc386707ac7a3ce714
JPotterf <Jacob.Potterf@gmail.com>
2020-11-10 21:51:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React COVID Tracker
+Built with create-react-app, uses leaflet and chart.js libraries.
+
+Covid-19 data source: https://disease.sh/v3/covid-19/
+
---
+++ src/Map.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Map as LeafletMap, TileLayer, GeoJSON } from "react-leaflet";
+import "./Map.css";
+import { showDataOnMap } from "./util";
+import countries_map from './countries.json';
+
+
+function Map({ countries, casesType, center, zoom }) {
+
+
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        <GeoJSON className='geojson'
+          data={countries_map}
+          
+          // style={this.geoJSONStyle}
+          // onEachFeature={this.onEachFeature}
+        />
+
+        {showDataOnMap(countries, casesType)}
+      </LeafletMap>
+    </div>
+  );
+}
+
+export default Map;
